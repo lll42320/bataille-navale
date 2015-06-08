b85b4e2f11b9f50ea6f4bb31d1c6d9c666c8edcf
lll42320 <lll42320@gmail.com>
2015-06-08 22:10:12 +0200

    Update Bataille.cpp

--- Bataille.cpp
+++ Bataille.cpp
@@ -31,3 +31,29 @@ void Bataille::play()
 	}
 	cout << "You win" << endl;
 }	 
+
+void Bataille::turn()
+{
+	int X=11 ;
+	while( x<1||x>10 )
+	{	
+		cout << "Sur quelle ligne voulez vous jouer? [1;10]" << endl;
+		cin >> x;
+	}
+	
+	While( y<1||y>10 )
+	{
+		cout << "Sur quelle colonne voulez vous jouer? [1;10]" << endl ;
+		cin >> y;
+	}
+
+	if (map [x][y]==0)
+	{
+		map [x][y]=2
+	}
+	if (map [x][y]==1)
+	{
+		map [x][y]=3
+	}
+}
+
